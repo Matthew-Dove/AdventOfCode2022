2c6e842d02cd860b3c1ae7e766da0221821eec69
Matthew Dove <matthew.dove@globalpayaunz.cloud>
2022-12-10 00:23:44 +1000

    Completed day 03.

--- src/Aoc22/Days/Day03.cs
+++ src/Aoc22/Days/Day03.cs
@@ -1,12 +1,70 @@
-﻿namespace Aoc22.Days;
+﻿using System.Collections.Generic;
+
+namespace Aoc22.Days;
 
 /**
  * https://adventofcode.com/2022/day/3
 **/
 public class Day03
 {
-    public int Parse(string calories)
+    public char[] ParsePartOne(string supplies)
     {
-        return 0;
+        var rucksacks = supplies.Replace("\r", string.Empty).Split('\n');
+        var priorities = new char[rucksacks.Length];
+
+        for (int i = 0; i < rucksacks.Length; i++)
+        {
+            var midpoint = rucksacks[i].Length / 2;
+            string compartmentA = rucksacks[i].Substring(0, midpoint), compartmentB = rucksacks[i].Substring(midpoint, rucksacks[i].Length - midpoint);
+
+            var hashset = new HashSet<char>(compartmentA);
+
+            foreach (var item in compartmentB)
+            {
+                if (hashset.Contains(item))
+                {
+                    priorities[i] = item;
+                    break;
+                }
+            }
+        }
+
+        return priorities;
+    }
+
+    public char[] ParsePartTwo(string supplies)
+    {
+        var rucksacks = supplies.Replace("\r", string.Empty).Split('\n');
+        var priorities = new char[rucksacks.Length / 3];
+
+        for (int i = 0, j = 0; i < rucksacks.Length; i+=3, j++)
+        {
+            string rucksackA = rucksacks[i + 0], rucksackB = rucksacks[i + 1], rucksackC = rucksacks[i + 2];
+
+            HashSet<char> hashsetA = new HashSet<char>(rucksackA), hashsetB = new HashSet<char>(rucksackB);
+
+            foreach (var item in rucksackC)
+            {
+                if (hashsetA.Contains(item) && hashsetB.Contains(item))
+                {
+                    priorities[j] = item;
+                    break;
+                }
+            }
+        }
+
+        return priorities;
+    }
+
+    public int Score(char[] priorities)
+    {
+        var score = 0;
+
+        foreach (var item in priorities)
+        {
+            score += item - (char.IsLower(item) ? 96 : 38);
+        }
+
+        return score;
     }
 }
--- src/Unit.Tests.Aoc22/Days/Day03Tests.cs
+++ src/Unit.Tests.Aoc22/Days/Day03Tests.cs
@@ -6,7 +6,12 @@
 [TestClass]
 public class Day03Tests
 {
-    private const string _exampleData = @"";
+    private const string _exampleData = @"vJrwpWtwJgWrhcsFMMfFFhFp
+jqHRNqRjqzjGDLGLrsFMfFZSrLrFZsSL
+PmmdzqPrVvPwwTWBwg
+wMqvLMZHhHMvwLHjbvcjnnSBnvTQFn
+ttgJtRGJQctTZtZT
+CrZsJsPPZsGzwwsLwLmpwMDw";
 
     private Day03 _day03;
 
@@ -19,24 +24,44 @@ public class Day03Tests
     [TestMethod]
     public void PartOneExample()
     {
-        
+        var supplies = _exampleData;
+
+        var priorities = _day03.ParsePartOne(supplies);
+        var score = _day03.Score(priorities);
+
+        Assert.AreEqual(157, score);
     }
 
     [TestMethod]
     public void PartOnePuzzle()
     {
-        
+        var supplies = File.ReadAllText("./Data/day03.txt");
+
+        var priorities = _day03.ParsePartOne(supplies);
+        var score = _day03.Score(priorities);
+
+        Assert.AreEqual(8493, score);
     }
 
     [TestMethod]
     public void PartTwoExample()
     {
-        
+        var supplies = _exampleData;
+
+        var priorities = _day03.ParsePartTwo(supplies);
+        var score = _day03.Score(priorities);
+
+        Assert.AreEqual(70, score);
     }
 
     [TestMethod]
     public void PartTwoPuzzle()
     {
-        
+        var supplies = File.ReadAllText("./Data/day03.txt");
+
+        var priorities = _day03.ParsePartTwo(supplies);
+        var score = _day03.Score(priorities);
+
+        Assert.AreEqual(2552, score);
     }
 }
