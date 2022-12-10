b1577eae3d0d60bf013d0ff43ed9ba2a43335b5e
Matthew Dove <matthew.dove@globalpayaunz.cloud>
2022-12-10 19:04:09 +1000

    Completed day 05.

--- src/Aoc22/Days/Day05.cs
+++ src/Aoc22/Days/Day05.cs
@@ -1,13 +1,96 @@
-﻿namespace Aoc22.Days;
+﻿using System.Diagnostics;
+
+namespace Aoc22.Days;
 
 /**
- * https://adventofcode.com/2022/day/1
+ * https://adventofcode.com/2022/day/5
 **/
 public class Day05
 {
-    public int[] Parse(string input)
+    public Cargo Parse(string input)
     {
-        
-        return null;
+        var parition = input.Replace("\r", string.Empty).Split("\n 1");
+        var stacks = parition[0].Split('\n');
+        var tmp = parition[1].Split('\n');
+        var stackCount = int.Parse(tmp.First().TrimEnd().Last().ToString());
+        var algorithm = tmp.Skip(2).ToArray();
+
+        var crates = new Stack<char>[stackCount];
+
+        for (int i = 0; i < stackCount; i++)
+        {
+            crates[i] = new Stack<char>(stacks.Length);
+            var offset = i * 4 + 1;
+
+            for (int j = stacks.Length - 1, k = 0; j >= 0; j--, k++)
+            {
+                var crate = stacks[j][offset];
+                if (crate != ' ') crates[i].Push(crate);
+            }
+        }
+
+        var instructions = new Instruction[algorithm.Length];
+
+        for (int i = 0; i < instructions.Length; i++)
+        {
+            var split = algorithm[i].Substring(5).Split(" from ");
+            var move = split[1].Split(" to ");
+            int take = int.Parse(split[0]), source = int.Parse(move[0]) - 1, target = int.Parse(move[1]) - 1;
+            instructions[i] = new Instruction { Take = take, Source = source, Target = target };
+        }
+
+        return new Cargo { Crates = crates, Instructions = instructions };
     }
+
+    public Cargo Move(Cargo cargo)
+    {
+        foreach (var instruction in cargo.Instructions)
+        {
+            for (int i = 0; i < instruction.Take; i++)
+            {
+                var crate = cargo.Crates[instruction.Source].Pop();
+                cargo.Crates[instruction.Target].Push(crate);
+            }
+        }
+
+        return cargo;
+    }
+
+    public Cargo MovePartTwo(Cargo cargo)
+    {
+        foreach (var instruction in cargo.Instructions)
+        {
+            var creates = new char[instruction.Take];
+            for (int i = 0; i < instruction.Take; i++) creates[i] = cargo.Crates[instruction.Source].Pop();
+            creates = creates.Reverse().ToArray();
+            for (int i = 0; i < instruction.Take; i++) cargo.Crates[instruction.Target].Push(creates[i]);
+        }
+
+        return cargo;
+    }
+
+    public string GetTopCrates(Cargo cargo)
+    {
+        var topCrates = string.Empty;
+
+        foreach (var crate in cargo.Crates)
+        {
+            topCrates += crate.Peek().ToString();
+        }
+
+        return topCrates;
+    }
+}
+
+public class Cargo
+{
+    public Stack<char>[] Crates { get; set; }
+    public Instruction[] Instructions { get; set; }
+}
+
+public class Instruction
+{
+    public int Take { get; set; }
+    public int Source { get; set; }
+    public int Target { get; set; }
 }
--- src/Unit.Tests.Aoc22/Days/Day05Tests.cs
+++ src/Unit.Tests.Aoc22/Days/Day05Tests.cs
@@ -6,7 +6,15 @@
 [TestClass]
 public class Day05Tests
 {
-    private const string _exampleData = @"";
+    private const string _exampleData = @"    [D]    
+[N] [C]    
+[Z] [M] [P]
+ 1   2   3 
+
+move 1 from 2 to 1
+move 3 from 1 to 3
+move 2 from 2 to 1
+move 1 from 1 to 2";
 
     private Day05 _day05;
 
@@ -19,24 +27,48 @@ public class Day05Tests
     [TestMethod]
     public void PartOneExample()
     {
+        var input = _exampleData;
 
+        var cargo = _day05.Parse(input);
+        cargo = _day05.Move(cargo);
+        var crates = _day05.GetTopCrates(cargo);
+
+        Assert.AreEqual("CMZ", crates);
     }
 
     [TestMethod]
     public void PartOnePuzzle()
     {
+        var input = File.ReadAllText("./Data/day05.txt");
 
+        var cargo = _day05.Parse(input);
+        cargo = _day05.Move(cargo);
+        var crates = _day05.GetTopCrates(cargo);
+
+        Assert.AreEqual("RNZLFZSJH", crates);
     }
 
     [TestMethod]
     public void PartTwoExample()
     {
+        var input = _exampleData;
 
+        var cargo = _day05.Parse(input);
+        cargo = _day05.MovePartTwo(cargo);
+        var crates = _day05.GetTopCrates(cargo);
+
+        Assert.AreEqual("MCD", crates);
     }
 
     [TestMethod]
     public void PartTwoPuzzle()
     {
+        var input = File.ReadAllText("./Data/day05.txt");
 
+        var cargo = _day05.Parse(input);
+        cargo = _day05.MovePartTwo(cargo);
+        var crates = _day05.GetTopCrates(cargo);
+
+        Assert.AreEqual("CNSFCGJSM", crates);
     }
 }
