32b25ca11a073298c0a040bc55fc56ec87c68da3
Matthew Dove <matthew.dove@globalpayaunz.cloud>
2022-12-10 14:47:12 +1000

    Completed day 04.

--- src/Aoc22/Days/Day04.cs
+++ src/Aoc22/Days/Day04.cs
@@ -5,8 +5,71 @@
 **/
 public class Day04
 {
-    public int Parse(string calories)
+    public Pair[] Parse(string assignments)
     {
-        return 0;
+        var sections = assignments.Replace("\r", string.Empty).Split('\n');
+        var pairs = new Pair[sections.Length];
+
+        for (int i = 0; i < sections.Length; i++)
+        {
+            var pair = sections[i].Split(',');
+            string[] sectionA = pair[0].Split('-'), sectionB = pair[1].Split('-');
+            Section
+                a = new Section { Start = int.Parse(sectionA[0]), End = int.Parse(sectionA[1]) },
+                b = new Section { Start = int.Parse(sectionB[0]), End = int.Parse(sectionB[1]) }
+            ;
+            pairs[i] = new Pair { A = a, B = b };
+        }
+
+        return pairs;
     }
+
+    public int CountContainedPairs(Pair[] pairs)
+    {
+        var matches = 0;
+
+        foreach (var pair in pairs)
+        {
+            if (pair.A.Start >= pair.B.Start && pair.A.End <= pair.B.End) matches++;
+            else if (pair.B.Start >= pair.A.Start && pair.B.End <= pair.A.End) matches++;
+        }
+
+        return matches;
+    }
+
+    public int CountOverlappingPairs(Pair[] pairs)
+    {
+        var matches = 0;
+
+        foreach (var pair in pairs)
+        {
+            var hash = new HashSet<int>();
+            for (int i = pair.A.Start; i <= pair.A.End; i++)
+            {
+                hash.Add(i);
+            }
+            for (int j = pair.B.Start; j <= pair.B.End; j++)
+            {
+                if (hash.Contains(j))
+                {
+                    matches++;
+                    break;
+                }
+            }
+        }
+
+        return matches;
+    }
+}
+
+public record Section
+{
+    public int Start { get; set; }
+    public int End { get; set; }
+}
+
+public record Pair
+{
+    public Section A { get; set; }
+    public Section B { get; set; }
 }
--- src/Unit.Tests.Aoc22/Days/Day04Tests.cs
+++ src/Unit.Tests.Aoc22/Days/Day04Tests.cs
@@ -6,7 +6,12 @@
 [TestClass]
 public class Day04Tests
 {
-    private const string _exampleData = @"";
+    private const string _exampleData = @"2-4,6-8
+2-3,4-5
+5-7,7-9
+2-8,3-7
+6-6,4-6
+2-6,4-8";
 
     private Day04 _day04;
 
@@ -19,24 +24,44 @@ public class Day04Tests
     [TestMethod]
     public void PartOneExample()
     {
+        var assignments = _exampleData;
 
+        var pairs = _day04.Parse(assignments);
+        var matches = _day04.CountContainedPairs(pairs);
+
+        Assert.AreEqual(2, matches);
     }
 
     [TestMethod]
     public void PartOnePuzzle()
     {
-        
+        var assignments = File.ReadAllText("./Data/day04.txt"); ;
+
+        var pairs = _day04.Parse(assignments);
+        var matches = _day04.CountContainedPairs(pairs);
+
+        Assert.AreEqual(524, matches);
     }
 
     [TestMethod]
     public void PartTwoExample()
     {
-        
+        var assignments = _exampleData;
+
+        var pairs = _day04.Parse(assignments);
+        var matches = _day04.CountOverlappingPairs(pairs);
+
+        Assert.AreEqual(4, matches);
     }
 
     [TestMethod]
     public void PartTwoPuzzle()
     {
-        
+        var assignments = File.ReadAllText("./Data/day04.txt"); ;
+
+        var pairs = _day04.Parse(assignments);
+        var matches = _day04.CountOverlappingPairs(pairs);
+
+        Assert.AreEqual(798, matches);
     }
 }
