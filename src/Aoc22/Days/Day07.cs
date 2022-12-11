b5fdb01912e8ca1d6a91bae8ebb931d8ea84b5cd
Matthew Dove <matthew.dove@globalpayaunz.cloud>
2022-12-11 18:14:56 +1000

    Completed day 07.

--- src/Aoc22/Days/Day07.cs
+++ src/Aoc22/Days/Day07.cs
@@ -5,8 +5,125 @@
 **/
 public class Day07
 {
-    public int[] Parse(string input)
+    public List<StdOut> Parse(string input)
     {
-        return null;
+        var stdout = input.Replace("\r", string.Empty).Split('\n');
+        var cmds = new List<StdOut>();
+
+        for (int i = 0; i < stdout.Length; i++)
+        {
+            if (stdout[i].Equals("$ cd /")) cmds.Add(new StdOut { Command = Cmd.ChangeDirectoryMoveRoot });
+            else if (stdout[i].Equals("$ cd ..")) cmds.Add(new StdOut { Command = Cmd.ChangeDirectoryMoveOut });
+            else if (stdout[i].StartsWith("$ cd ")) cmds.Add(new StdOut { Command = Cmd.ChangeDirectoryMoveIn, Directory = stdout[i].Split("$ cd ")[1] });
+            else if (stdout[i].Equals("$ ls"))
+            {
+                var contents = new List<Content>();
+                for (++i; i < stdout.Length; i++)
+                {
+                    if (stdout[i].StartsWith("$")) break;
+                    if (stdout[i].StartsWith("dir ")) contents.Add(new Content { ListType = LsType.Directory, Name = stdout[i].Split("dir ")[1] });
+                    else contents.Add(new Content { ListType = LsType.File, Size = int.Parse(stdout[i].Split(" ")[0]), Name = stdout[i].Split(" ")[1] });
+                }
+                i--;
+                cmds.Add(new StdOut { Command = Cmd.List, Contents = contents });
+            }
+        }
+
+        return cmds;
     }
+
+    public Directory CreateDirectory(List<StdOut> stdout)
+    {
+        var directory = new Directory("/", null);
+
+        foreach (var std in stdout.Skip(1))
+        {
+            if (std.Command == Cmd.ChangeDirectoryMoveIn) directory = directory.MoveIn(std.Directory);
+            else if (std.Command == Cmd.ChangeDirectoryMoveOut) directory = directory.MoveOut();
+            else if (std.Command == Cmd.ChangeDirectoryMoveRoot) directory = directory.MoveRoot();
+            else if (std.Command == Cmd.List)
+            {
+                foreach (var content in std.Contents)
+                {
+                    if (content.ListType == LsType.Directory) directory.AddDirectory(content.Name);
+                    else if (content.ListType == LsType.File) directory.AddFile(content.Name, content.Size);
+                }
+            }
+        }
+
+        return directory.MoveRoot();
+    }
+
+    public int GetSumOfDirectories(Directory directory, int maxDirectorySize)
+    {
+        int size = directory.GetSize(), sum = 0;
+        if (size <= maxDirectorySize) sum += size;
+        foreach (var dir in directory.Directories.Values) sum += GetSumOfDirectories(dir, maxDirectorySize);
+        return sum;
+    }
+
+    public int GetClosetDirectorySize(Directory directory, int targetSize)
+    {
+        var matches = new List<int>();
+
+        void traverse(Directory dir)
+        {
+            var size = dir.GetSize();
+            if (size >= targetSize) matches.Add(size);
+            foreach (var d in dir.Directories.Values) traverse(d);
+        }
+
+        traverse(directory);
+        return matches.OrderBy(x => x).First();
+    }
+}
+
+public class StdOut
+{
+    public Cmd Command { get; set; }
+    public string Directory { get; set; }
+    public List<Content> Contents { get; set; }
+}
+
+public enum Cmd { ChangeDirectoryMoveIn, ChangeDirectoryMoveOut, ChangeDirectoryMoveRoot, List }
+public enum LsType { Directory, File }
+
+public class Content
+{
+    public LsType ListType { get; set; }
+    public string Name { get; set; }
+    public int Size { get; set; }
+}
+
+public class Directory
+{
+    public Directory(string name, Directory parent) { Name = name; Parent = parent; }
+    public Directory Parent { get; set; }
+    public string Name { get; set; }
+    public Dictionary<string, Directory> Directories { get; set; } = new Dictionary<string, Directory>();
+    public Dictionary<string, FileModel> Files { get; set; } = new Dictionary<string, FileModel>();
+
+    public void AddDirectory(string name) => Directories[name] = new Directory(name, this);
+
+    public void AddFile(string name, int size) => Files[name] = new FileModel(name, size);
+
+    public Directory MoveIn(string name) => Directories[name];
+
+    public Directory MoveOut() => Parent;
+
+    public Directory MoveRoot()
+    {
+        Directory root = this;
+        while (root.Parent != null) root = root.Parent;
+        return root;
+    }
+
+    public int GetSize() => Files.Values.Sum(x => x.Size) + Directories.Values.Select(x => x.GetSize()).Sum();
+}
+
+public class FileModel
+{
+    public FileModel(string name, int size) { Name = name; Size = size; }
+    public string Name { get; set; }
+    public int Size { get; set; }
 }
--- src/Unit.Tests.Aoc22/Days/Day07Tests.cs
+++ src/Unit.Tests.Aoc22/Days/Day07Tests.cs
@@ -6,7 +6,29 @@
 [TestClass]
 public class Day07Tests
 {
-    private const string _exampleData = @"";
+    private const string _exampleData = @"$ cd /
+$ ls
+dir a
+14848514 b.txt
+8504156 c.dat
+dir d
+$ cd a
+$ ls
+dir e
+29116 f
+2557 g
+62596 h.lst
+$ cd e
+$ ls
+584 i
+$ cd ..
+$ cd ..
+$ cd d
+$ ls
+4060174 j
+8033020 d.log
+5626152 d.ext
+7214296 k";
 
     private Day07 _day07;
 
@@ -21,24 +43,50 @@ public class Day07Tests
     {
         var input = _exampleData;
 
-        var output = _day07.Parse(input);
+        var stdout = _day07.Parse(input);
+        var directory = _day07.CreateDirectory(stdout);
+        var size = _day07.GetSumOfDirectories(directory, 100000);
+
+        Assert.AreEqual(95437, size);
     }
 
     [TestMethod]
     public void PartOnePuzzle()
     {
-        
+        var input = File.ReadAllText("./Data/day07.txt");
+
+        var stdout = _day07.Parse(input);
+        var directory = _day07.CreateDirectory(stdout);
+        var size = _day07.GetSumOfDirectories(directory, 100000);
+
+        Assert.AreEqual(1427048, size);
     }
 
     [TestMethod]
     public void PartTwoExample()
     {
-        
+        const int fileSystemSize = 70000000, updateSize = 30000000;
+        var input = _exampleData;
+
+        var stdout = _day07.Parse(input);
+        var directory = _day07.CreateDirectory(stdout);
+        var targetSize = updateSize - (fileSystemSize - directory.GetSize());
+        var size = _day07.GetClosetDirectorySize(directory, targetSize);
+
+        Assert.AreEqual(24933642, size);
     }
 
     [TestMethod]
     public void PartTwoPuzzle()
     {
-        
+        const int fileSystemSize = 70000000, updateSize = 30000000;
+        var input = File.ReadAllText("./Data/day07.txt");
+
+        var stdout = _day07.Parse(input);
+        var directory = _day07.CreateDirectory(stdout);
+        var targetSize = updateSize - (fileSystemSize - directory.GetSize());
+        var size = _day07.GetClosetDirectorySize(directory, targetSize);
+
+        Assert.AreEqual(2940614, size);
     }
 }
