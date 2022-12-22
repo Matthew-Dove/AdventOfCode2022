6fcd44097ccd34afde38e301e4fcaf6c2ec01aff
Matthew Dove <matthew.dove@globalpayaunz.cloud>
2022-12-22 17:52:12 +1000

    Day 10 part one.

--- src/Aoc22/Days/Day10.cs
+++ src/Aoc22/Days/Day10.cs
@@ -5,8 +5,49 @@
 **/
 public class Day10
 {
-    public int[] Parse(string input)
+    public Program[] Parse(string input) => input.Replace("\r", string.Empty).Split('\n').Select(x => new Program(x)).ToArray();
+
+    public int GetSignalStrength(Program[] program, params int[] targetCycles)
     {
-        return null;
+        var sum = 0;
+
+        for (int i = 0; i < targetCycles.Length; i++)
+        {
+            var cycleValue = GetCycleValue(program, targetCycles[i]);
+            var signalStrength = cycleValue * targetCycles[i];
+            sum += signalStrength;
+        }
+
+        return sum;
+    }
+
+    private int GetCycleValue(Program[] program, int targetCycle)
+    {
+        int cycles = 0, register = 1;
+
+        for (int i = 0; i < program.Length; i++)
+        {
+            cycles += program[i].Cycles;
+            register += program[i].Value;
+
+            if (cycles >= targetCycle) return register - program[i].Value;
+        }
+
+        return 0;
+    }
+}
+
+public record Program
+{
+    public int Cycles { get; } = 1;
+    public int Value { get; } = 0;
+
+    public Program(string instruction)
+    {
+        if (instruction[0] == 'a')
+        {
+            Cycles = 2;
+            Value = int.Parse(instruction["addx ".Length..]);
+        }
     }
 }
--- src/Unit.Tests.Aoc22/Days/Day10Tests.cs
+++ src/Unit.Tests.Aoc22/Days/Day10Tests.cs
@@ -6,7 +6,152 @@
 [TestClass]
 public class Day10Tests
 {
-    private const string _exampleData = @"";
+    private const string _exampleData = @"addx 15
+addx -11
+addx 6
+addx -3
+addx 5
+addx -1
+addx -8
+addx 13
+addx 4
+noop
+addx -1
+addx 5
+addx -1
+addx 5
+addx -1
+addx 5
+addx -1
+addx 5
+addx -1
+addx -35
+addx 1
+addx 24
+addx -19
+addx 1
+addx 16
+addx -11
+noop
+noop
+addx 21
+addx -15
+noop
+noop
+addx -3
+addx 9
+addx 1
+addx -3
+addx 8
+addx 1
+addx 5
+noop
+noop
+noop
+noop
+noop
+addx -36
+noop
+addx 1
+addx 7
+noop
+noop
+noop
+addx 2
+addx 6
+noop
+noop
+noop
+noop
+noop
+addx 1
+noop
+noop
+addx 7
+addx 1
+noop
+addx -13
+addx 13
+addx 7
+noop
+addx 1
+addx -33
+noop
+noop
+noop
+addx 2
+noop
+noop
+noop
+addx 8
+noop
+addx -1
+addx 2
+addx 1
+noop
+addx 17
+addx -9
+addx 1
+addx 1
+addx -3
+addx 11
+noop
+noop
+addx 1
+noop
+addx 1
+noop
+noop
+addx -13
+addx -19
+addx 1
+addx 3
+addx 26
+addx -30
+addx 12
+addx -1
+addx 3
+addx 1
+noop
+noop
+noop
+addx -9
+addx 18
+addx 1
+addx 2
+noop
+noop
+addx 9
+noop
+noop
+noop
+addx -1
+addx 2
+addx -37
+addx 1
+addx 3
+noop
+addx 15
+addx -21
+addx 22
+addx -6
+addx 1
+noop
+addx 2
+addx 1
+noop
+addx -10
+noop
+noop
+addx 20
+addx 1
+addx 2
+addx 2
+addx -6
+addx -11
+noop
+noop
+noop";
 
     private Day10 _day10;
 
@@ -21,13 +166,21 @@ public class Day10Tests
     {
         var input = _exampleData;
 
-        var output = _day10.Parse(input);
+        var program = _day10.Parse(input);
+        var strength = _day10.GetSignalStrength(program, 20, 60, 100, 140, 180, 220);
+
+        Assert.AreEqual(13140, strength);
     }
 
     [TestMethod]
     public void PartOnePuzzle()
     {
+        var input = File.ReadAllText("./Data/day10.txt");
 
+        var program = _day10.Parse(input);
+        var strength = _day10.GetSignalStrength(program, 20, 60, 100, 140, 180, 220);
+
+        Assert.AreEqual(12880, strength);
     }
 
     [TestMethod]
